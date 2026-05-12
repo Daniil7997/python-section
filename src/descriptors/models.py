474e25df2f98ef1818c94caa0597c55e9313ee45
Daniil Panteleev <panteleev1999@list.ru>
2026-05-13 02:12:08 +0300

    2 task is solved

--- src/descriptors/models.py
+++ src/descriptors/models.py
@@ -1,12 +1,27 @@
-from typing import Any, TypeAlias
+class Field:
+    def __init__(self, path: str):
+        self.path = path.split('.')
 
-JSON: TypeAlias = dict[str, Any]
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+
+        data = instance.payload
+        for key in self.path:
+            try:
+                data = data[key]
+            except (KeyError, TypeError):
+                return None
+        return data
+
+    def __set__(self, instance, value):
+        data = instance.payload
+        parts = self.path
+        for key in parts[:-1]:
+            data = data[key]
+        data[parts[-1]] = value
 
 
 class Model:
-    def __init__(self, payload: JSON):
+    def __init__(self, payload: dict):
         self.payload = payload
-
-
-class Field:
-    pass
